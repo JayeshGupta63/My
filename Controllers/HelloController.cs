8ace3dbe4501dbb1dc8cd0bb62acc87c4adb3da1
jayeshgupta63 <jayeshgupta63@gmail.com>
2023-05-08 09:30:27 +0530

    Initial Commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MS_Prac.Models;
+
+namespace MS_Prac.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("");
+
+
+        }
+
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Jayesh Gupta",
+                Gender = "Male"
+            };
+
+
+        return View(student);
+
+        }
+
+    }
+
+}
